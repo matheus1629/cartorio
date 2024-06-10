56dbf7e57b56593835b5ca3b5654ee06bc0700d9
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2024-06-10 10:02:06 -0300

    initial commit

---
+++ src/test/java/com/escriba/cartorio/CartorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escriba.cartorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
